41976d19c5530d08ec26a44842af6f1fabdb8be8
Syd Xu <prof.syd.xu@gmail.com>
2021-06-25 15:57:42 +0800

    ...

--- model/wdk/HMResult.go
+++ model/wdk/HMResult.go
@@ -1,18 +1,27 @@
 package wdk
 
-// HMResult 
-type HMResult struct {
+// HmResult 
+type HmResult struct {
 
-    // 配置信息列表
-    Model   *ConveyorBasicConfigDTO `json:"model,omitempty"`
-
-    // 是否成功
-    Success   bool `json:"success,omitempty"`
+    // 设备列表
+    Models   []DeviceInfoDto `json:"models,omitempty"`
 
     // 错误码
     ErrorCode   string `json:"error_code,omitempty"`
 
     // 错误信息
     ErrorMsg   string `json:"error_msg,omitempty"`
+
+    // 是否成功
+    Success   bool `json:"success,omitempty"`
+
+    // model
+    Model   *MqttDeviceInfoDto `json:"model,omitempty"`
+
+    // msgCode
+    MsgCode   string `json:"msg_code,omitempty"`
+
+    // msgInfo
+    MsgInfo   string `json:"msg_info,omitempty"`
 
 }
--- model/wdk/SkuQueryDo.go
+++ model/wdk/SkuQueryDo.go
@@ -1,12 +1,18 @@
 package wdk
 
-// SkuQueryDo 
-type SkuQueryDo struct {
+// SkuQueryDO 
+type SkuQueryDO struct {
 
-    // 商品编码列表
-    SkuCodes   []String `json:"sku_codes,omitempty"`
-
-    // 门店或DC编码
+    // 门店或DC编码,如果填写了渠道店id，该字段会被忽略
     OuCode   string `json:"ou_code,omitempty"`
+
+    // 渠道店id
+    ShopId   string `json:"shop_id,omitempty"`
+
+    // 渠道编码（默认4）,如果填写了渠道店id，该字段会被忽略
+    ChannelCode   string `json:"channel_code,omitempty"`
+
+    // 商品编码（多个条码用英文逗号隔开，最多支持20个）
+    SkuCodes   []String `json:"sku_codes,omitempty"`
 
 }
