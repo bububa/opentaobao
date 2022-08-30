50c118cb5785e3103580288be63db6c9f19ff206
Syd Xu <prof.syd.xu@gmail.com>
2022-08-30 17:31:19 +0800

    fix tests

--- tests/xml_test.go
+++ tests/xml_test.go
@@ -13,12 +13,13 @@ import (
 func TestXMLFormatHmac(t *testing.T) {
 	appKey := os.Getenv("APPKEY")
 	secret := os.Getenv("SECRET")
+	testNumIids := os.Getenv("TEST_NUM_IIDS")
 	clt := core.NewSDKClient(appKey, secret)
 	clt.SetDebug(true)
 	clt.SetAPIFormat(model.XML)
 	clt.SetSignMethod(model.HMAC)
 	req := tbkModel.NewTaobaoTbkItemInfoGetRequest()
-	req.SetNumIids("617361236670")
+	req.SetNumIids(testNumIids)
 	req.SetPlatform(2)
 	resp, err := tbkApi.TaobaoTbkItemInfoGet(clt, req, "")
 	if err != nil {
@@ -31,12 +32,13 @@ func TestXMLFormatHmac(t *testing.T) {
 func TestXMLFormatMd5(t *testing.T) {
 	appKey := os.Getenv("APPKEY")
 	secret := os.Getenv("SECRET")
+	testNumIids := os.Getenv("TEST_NUM_IIDS")
 	clt := core.NewSDKClient(appKey, secret)
 	clt.SetDebug(true)
 	clt.SetAPIFormat(model.XML)
 	clt.SetSignMethod(model.MD5)
 	req := tbkModel.NewTaobaoTbkItemInfoGetRequest()
-	req.SetNumIids("617361236670")
+	req.SetNumIids(testNumIids)
 	req.SetPlatform(2)
 	resp, err := tbkApi.TaobaoTbkItemInfoGet(clt, req, "")
 	if err != nil {
