af4bb697ec7f69e6b58fbe6a81fb3dfc3cdf9af8
Syd Xu <prof.syd.xu@gmail.com>
2025-05-26 12:21:27 +0800

    feat(udsmart): add alibaba.taobao.ud.smart.order.collect.cart.detail.pull

---
+++ model/tbtrade/AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest.go
@@ -0,0 +1,78 @@
+package tbtrade
+
+import (
+	"net/url"
+	"sync"
+
+	"github.com/bububa/opentaobao/model"
+)
+
+// AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest UD效果外投收藏加购明细拉取 API请求
+// alibaba.taobao.ud.smart.order.collect.cart.detail.pull
+//
+// UD效果外投收藏加购明细拉取
+type AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest struct {
+	model.Params
+	// 查询入参
+	_orderTopCursorQueryDto *OrderTopCursorQueryDto
+}
+
+// NewAlibabaTaobaoUdSmartOrderCollectCartDetailPullRequest 初始化AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest对象
+func NewAlibabaTaobaoUdSmartOrderCollectCartDetailPullRequest() *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest {
+	return &AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest{
+		Params: model.NewParams(1),
+	}
+}
+
+// Reset IRequest interface 方法, 清空结构体
+func (r *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest) Reset() {
+	r._orderTopCursorQueryDto = nil
+	r.Params.ToZero()
+}
+
+// GetApiMethodName IRequest interface 方法, 获取Api method
+func (r AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest) GetApiMethodName() string {
+	return "alibaba.taobao.ud.smart.order.collect.cart.detail.pull"
+}
+
+// GetApiParams IRequest interface 方法, 获取API参数
+func (r AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest) GetApiParams(params url.Values) {
+	for k, v := range r.Params {
+		params.Set(k, v.String())
+	}
+}
+
+// GetRawParams IRequest interface 方法, 获取API原始参数
+func (r AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest) GetRawParams() model.Params {
+	return r.Params
+}
+
+// SetOrderTopCursorQueryDto is OrderTopCursorQueryDto Setter
+// 查询入参
+func (r *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest) SetOrderTopCursorQueryDto(_orderTopCursorQueryDto *OrderTopCursorQueryDto) error {
+	r._orderTopCursorQueryDto = _orderTopCursorQueryDto
+	r.Set("order_top_cursor_query_dto", _orderTopCursorQueryDto)
+	return nil
+}
+
+// GetOrderTopCursorQueryDto OrderTopCursorQueryDto Getter
+func (r AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest) GetOrderTopCursorQueryDto() *OrderTopCursorQueryDto {
+	return r._orderTopCursorQueryDto
+}
+
+var poolAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest = sync.Pool{
+	New: func() any {
+		return NewAlibabaTaobaoUdSmartOrderCollectCartDetailPullRequest()
+	},
+}
+
+// GetAlibabaTaobaoUdSmartOrderCollectCartDetailPullRequest 从 sync.Pool 获取 AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest
+func GetAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest() *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest {
+	return poolAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest.Get().(*AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest)
+}
+
+// ReleaseAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest 将 AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest 放入 sync.Pool
+func ReleaseAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest(v *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest) {
+	v.Reset()
+	poolAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIRequest.Put(v)
+}
---
+++ model/tbtrade/AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse.go
@@ -0,0 +1,67 @@
+package tbtrade
+
+import (
+	"encoding/xml"
+	"sync"
+
+	"github.com/bububa/opentaobao/model"
+)
+
+// AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse UD效果外投收藏加购明细拉取 API返回值
+// alibaba.taobao.ud.smart.order.collect.cart.detail.pull
+//
+// UD效果外投收藏加购明细拉取
+type AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse struct {
+	model.CommonResponse
+	AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponseModel
+}
+
+// Reset 清空结构体
+func (m *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse) Reset() {
+	(&m.CommonResponse).Reset()
+	(&m.AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponseModel).Reset()
+}
+
+// AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponseModel is UD效果外投收藏加购明细拉取 成功返回结果
+type AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponseModel struct {
+	XMLName xml.Name `xml:"alibaba_taobao_ud_smart_order_collect_cart_detail_pull_response"`
+	// 平台颁发的每次请求访问的唯一标识
+	RequestId string `json:"request_id,omitempty" xml:"request_id,omitempty"`
+	// 收藏加购明细数据
+	Result []OrderDetailCursorTopDto `json:"result,omitempty" xml:"result>order_detail_cursor_top_dto,omitempty"`
+	// 错误码
+	ErrorCode string `json:"error_code,omitempty" xml:"error_code,omitempty"`
+	// 错误信息
+	ErrorMsg string `json:"error_msg,omitempty" xml:"error_msg,omitempty"`
+	// 页码游标，下一次请求传入，实现增量查询
+	CursorId string `json:"cursor_id,omitempty" xml:"cursor_id,omitempty"`
+	// 是否成功
+	Success bool `json:"success,omitempty" xml:"success,omitempty"`
+}
+
+// Reset 清空结构体
+func (m *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponseModel) Reset() {
+	m.RequestId = ""
+	m.Result = m.Result[:0]
+	m.ErrorCode = ""
+	m.ErrorMsg = ""
+	m.CursorId = ""
+	m.Success = false
+}
+
+var poolAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse = sync.Pool{
+	New: func() any {
+		return new(AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse)
+	},
+}
+
+// GetAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse 从 sync.Pool 获取 AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse
+func GetAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse() *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse {
+	return poolAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse.Get().(*AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse)
+}
+
+// ReleaseAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse 将 AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse 保存到 sync.Pool
+func ReleaseAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse(v *AlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse) {
+	v.Reset()
+	poolAlibabaTaobaoUdSmartOrderCollectCartDetailPullAPIResponse.Put(v)
+}
